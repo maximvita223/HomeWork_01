56b51e0948ae2724274797f9738897aa8676f22b
Maxim <maks.bidenko.96@mail.ru>
2022-08-07 12:30:22 +0300

    Сделал 1 задание

---
+++ dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("введите 1 число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите 2 число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.Write($"Число {num1} больше {num2}");
+else
+    Console.Write($"Число {num1} меньше {num2}");
