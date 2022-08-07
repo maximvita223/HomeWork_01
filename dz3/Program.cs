da103a1d42fbf3755c389ff218b8da994417789e
Maxim <maks.bidenko.96@mail.ru>
2022-08-07 12:31:37 +0300

    Сделал 3 задание

---
+++ dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число : ");
+
+int a = Convert.ToInt32(Console.Read());
+                
+if (a % 2 == 0) 
+Console.Write("Четное");
+                
+else 
+Console.Write("Нечетное");
