9de4a014510fcf04a3463c818971f36eac883a3e
Maxim <maks.bidenko.96@mail.ru>
2022-08-07 12:31:11 +0300

    Сделал 2 задание

---
+++ dz2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("введите 1 число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите 2 число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите 3 число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write($"{max}");
+
+
