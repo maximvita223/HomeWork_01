0658c3a55e92069055ac3fa9208e401920812d0f
Maxim <maks.bidenko.96@mail.ru>
2022-08-07 12:32:18 +0300

    Сделал 4 задание

---
+++ dz4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("введите число: ");
+
+int N = Int32.Parse(Console.ReadLine());
+
+for (int x = 0; x < N; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
